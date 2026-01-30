df819f210caaa7ae4a0b0e5af3c13bf3366f29fd
FlashTheFire <harshtakur001@gmail.com>
2026-01-31 00:55:32 +0530

    chore: Disable ESLint during build

--- nexnum-app/next.config.mjs
+++ nexnum-app/next.config.mjs
@@ -15,6 +15,9 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 
     // CDN & Caching Optimization
     async headers() {
